a6fb9e581d5d3630042bdd162fb40b7bbb544484
Daniel James <daniel@thzinc.com>
2018-11-15 21:57:06 -0800

    Lots and lots of navigation work

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,27 @@
-document.querySelectorAll('nav .open')
-    .forEach(open => open.addEventListener('click', (ev) => {
-        document.body.classList.add(`show-${open.dataset.for}`);
-    }));
-document.querySelectorAll('nav .close')
-    .forEach(close => close.addEventListener('click', (ev) => {
-        document.body.classList.remove(`show-${close.dataset.for}`);
-    }));
-const scrollHandler = (ev) => {
-    if (ev.deltaY > 0) {
-        document.body.classList.add("collapse-main-navigation");
-    } else if (ev.deltaY < 0) {
-        document.body.classList.remove("collapse-main-navigation");
+document.querySelectorAll('nav .corner')
+  .forEach(corner => corner.addEventListener('click', (ev) => {
+    document.body.classList.toggle(`show-${corner.dataset.for}`);
+  }));
+
+const firstHero = document.querySelector('.hero:first-child');
+if (firstHero) {
+  let heroBottom;
+  const scrollHandler = () => {
+    if (window.scrollY > heroBottom) {
+      document.body.classList.add("fixed-main-navigation");
+    } else {
+      document.body.classList.remove("fixed-main-navigation");
     }
-};
-window.addEventListener('scroll', scrollHandler);
-window.addEventListener('wheel', scrollHandler);
+  };
+
+  const resizeHandler = () => {
+    const { bottom } = firstHero.getBoundingClientRect();
+    heroBottom = window.scrollY + bottom;
+    scrollHandler();
+  };
+  resizeHandler();
+
+  window.addEventListener('scroll', scrollHandler);
+  window.addEventListener('wheel', scrollHandler);
+  window.addEventListener('resize', resizeHandler);
+}
